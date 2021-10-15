88442ffc5f7da4bae3c04ffac8e6680fb6b5b519
FrancescoAndreotti <francescoandreotti00@gmail.com>
2021-10-15 16:55:33 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-const num = parseInt(prompt("Inserisci un numero"));
+/* const num = parseInt(prompt("Inserisci un numero"));
 
 if (num % 2 === 0) {
     console.log(num);
 } else {
     console.log(num + 1);
+} */
+
+
+const array1 = [15, 2, 7];
+const array2 = [5];
+
+
+while (array2.length < array1.length) {
+    array2.push(prompt("Inserisci un numero"));
 }
+
+console.log(array1, array2);
